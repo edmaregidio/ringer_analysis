6dd3a8b489ca9f4314d5b9b23ccb5c9f87b08468
jodafons <jodafons@lps.ufrj.br>
2020-12-31 16:14:07 -0300

    add more data extraction scripts

--- 2020/mc15_13TeV.sgn.probes_lhmedium_Zee.bkg.Truth.JF17.oldgrid/run_mc15_13TeV_Zee_oldGrid_collector.sh
+++ 2020/mc15_13TeV.sgn.probes_lhmedium_Zee.bkg.Truth.JF17.oldgrid/run_mc15_13TeV_Zee_oldGrid_collector.sh
@@ -1,8 +1,8 @@
 
 
-BASEPATH_ZEE=/afs/cern.ch/work/j/jodafons/public/mc15_13TeV/PhysVal_v2/user.jodafons.mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.AOD.e3601_s2876_r7917_r7676.PhysVal_v2
+BASEPATH_ZEE=~jodafons/public/cern_data/mc15_13TeV/PhysVal_v2/user.jodafons.mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.AOD.e3601_s2876_r7917_r7676.PhysVal_v2
 
-prun_jobs.py -i $BASEPATH_ZEE -c "python3 job_collector.py --oldGrid" -mt 60
+prun_jobs.py -i $BASEPATH_ZEE -c "python3 job_collector.py --oldGrid" -mt 40
 
 
 mkdir mc15_Zee
